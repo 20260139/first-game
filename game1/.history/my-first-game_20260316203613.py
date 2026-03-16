76c52dd6daba4bfcc8adf9ef5bf466283250a3e2
rupang101 <jh141500@gmail.com>
2026-03-16 20:52:16 +0900

    3rd commit

--- game1/.history/my-first-game_20260316203613.py
+++ game1/.history/my-first-game_20260316203613.py
@@ -14,59 +14,69 @@ particles = []
 
 class Particle:
     def __init__(self, x, y):
-
         self.x = x
         self.y = y
 
         angle = random.uniform(0, math.pi * 2)
-        speed = random.uniform(2, 7)
+        speed = random.uniform(2, 8)
 
         self.vx = math.cos(angle) * speed
         self.vy = math.sin(angle) * speed
 
-        self.life = random.randint(60, 100)
-        self.size = random.randint(3, 6)
+        self.life = random.randint(60, 120)
+        self.size = random.randint(3, 8)
 
         self.hue = random.randint(0, 360)
 
-    def update(self):
+        self.trail = []
 
+    def update(self):
         self.x += self.vx
         self.y += self.vy
 
         self.vy += 0.05
         self.life -= 1
 
-        self.hue += 3
+        self.hue += 2
+
+        self.trail.append((self.x, self.y))
+        if len(self.trail) > 10:
+            self.trail.pop(0)
 
     def get_color(self):
-
         r = int((math.sin(self.hue * 0.02) + 1) * 127)
         g = int((math.sin(self.hue * 0.02 + 2) + 1) * 127)
         b = int((math.sin(self.hue * 0.02 + 4) + 1) * 127)
-
         return (r, g, b)
 
     def draw(self, surf):
 
         color = self.get_color()
 
-        # glow
-        pygame.draw.circle(surf, color, (int(self.x), int(self.y)), self.size + 6)
-        pygame.draw.circle(surf, color, (int(self.x), int(self.y)), self.size + 3)
+        for i, pos in enumerate(self.trail):
+            alpha_size = int(self.size * (i / len(self.trail)))
+            pygame.draw.circle(
+                surf,
+                color,
+                (int(pos[0]), int(pos[1])),
+                alpha_size
+            )
 
-        # main particle
-        pygame.draw.circle(surf, (255,255,255), (int(self.x), int(self.y)), self.size)
+        pygame.draw.circle(
+            surf,
+            color,
+            (int(self.x), int(self.y)),
+            self.size
+        )
 
     def alive(self):
         return self.life > 0
 
 
 def draw_background(surface, t):
-
     for y in range(HEIGHT):
-        c = int(50 + 30 * math.sin(y * 0.02 + t))
-        color = (10, c, 80)
+        c = int(60 + 40 * math.sin(y * 0.02 + t))
+        color = (10, c, 90)
         pygame.draw.line(surface, color, (0, y), (WIDTH, y))
 
 
@@ -76,21 +86,18 @@ time = 0
 while running:
 
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
             running = False
 
-        # 큰 폭발
         if event.type == pygame.MOUSEBUTTONDOWN:
-            for _ in range(120):
+            for _ in range(80):
                 particles.append(Particle(event.pos[0], event.pos[1]))
 
     mouse = pygame.mouse.get_pos()
     buttons = pygame.mouse.get_pressed()
 
-    # 마우스 드래그 파티클
     if buttons[0]:
-        for _ in range(10):
+        for _ in range(12):
             particles.append(Particle(mouse[0], mouse[1]))
 
     time += 0.03
